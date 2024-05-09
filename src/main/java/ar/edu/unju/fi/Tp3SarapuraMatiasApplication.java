3d1f5b121c9d053c559e94ca3bc812ddcf2078eb
mathy <matiwordgit@gmail.com>
2024-05-09 16:22:18 -0300

    TP3 Asistencia

---
+++ src/main/java/ar/edu/unju/fi/Tp3SarapuraMatiasApplication.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SarapuraMatiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3SarapuraMatiasApplication.class, args);
+		
+	}
+
+}
